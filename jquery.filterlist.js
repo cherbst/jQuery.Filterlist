8c1a295af68c7a69579e25c4cd85ad3373c6bce2
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-09-15 00:11:08 +0200

    filterlist: new anim v2
    added stop/resume support

--- jquery.filterlist.js
+++ jquery.filterlist.js
@@ -20,8 +20,9 @@
 				container : null,
 				stickToCurrentElement : false,
 				curElement : null,
-				beforeShow : function(){},
-				afterShow : function(){},
+				margin: 0,
+				before : function(){},
+				after : function(){},
 				getNewCurElement : function(filter,oldCur,shown){ 
 					return (shown.index(oldCur)==-1?shown.first():oldCur); 
 				}
@@ -33,11 +34,14 @@
 				if ( !settings.curElement ){
 					settings.curElement = settings.container.data('curElement');
 					if ( !settings.curElement )
-						settings.curElement = this.first(':visible');
+						settings.curElement = this.filter(isVisible).first();
 				}
 				settings.stickToCurrentElement = true;
-				settings.margin = ( settings.horizontal?settings.curElement.offset().left:
-							settings.curElement.offset().top);
+
+				if ( settings.margin == 0 )
+					settings.margin = ( settings.horizontal?
+						settings.curElement.offset().left:
+						settings.curElement.offset().top);
 			}
 			settings.curElement = filterElements.apply(this,[settings]);
 			if ( settings.container )
@@ -51,27 +55,25 @@
 		if ( options.curElement.length == 0)
 			return 0;
 		
-		if ( toShow.index(options.curElement) == -1 )
-			return (options.horizontal?options.curElement.offset().left:options.curElement.offset().top) - options.margin;
-
-		var upper = options.curElement.prevAll(':visible').first();
-
-		if ( upper.length > 0 )
-			return (options.horizontal?upper.offset().left:upper.offset().top) + 
-				(options.horizontal?upper.width():upper.height()) - options.margin;
-
-		var closest = options.curElement.closest(':visible');
-		return (options.horizontal?closest.offset().left:closest.offset().top) - options.margin;
+		return (options.horizontal?
+			options.curElement.offset().left:options.curElement.offset().top) - options.margin;
 	}
 
 	// gets the extent of the element from its data field if possible
 	// this is done because getting the extent of a hidden element
 	// takes a long time
-	function getElementExtent(element,horizontal){
-		var dataExtent = element.data('extent');
+	function getElementExtent(element,horizontal,nocache){
+		var dataExtent = (nocache?null:element.data('extent'));
 		var extent = (dataExtent?parseInt(dataExtent,10):(horizontal?element.width():element.height()));
-		if ( !dataExtent ) element.data('extent',extent);
+		if ( !dataExtent && !nocache ) element.data('extent',extent);
 		return extent;
+	}
+
+	function setElementExtent(element,extent,horizontal){
+		if ( horizontal )
+			element.width(extent);
+		else
+			element.height(extent);
 	}
 
 	function getRefExtent(allElements,toHide,toShow,options){
@@ -79,20 +81,29 @@
 		var refExtent = 0;
 
 		toHide.each(function(){
-			extent = getElementExtent($(this),options.horizontal);
+			extent = getElementExtent($(this),options.horizontal,true);
 			if ( options.stickToCurrentElement && 
 				options.curElement.length > 0 && allElements.index($(this)) < curIndex )
 				refExtent -= extent;
 		});
 
 		toShow.each(function(){
-			extent = getElementExtent($(this),options.horizontal);
+			curExtent = getElementExtent($(this),options.horizontal,true);
+			extent = getElementExtent($(this),options.horizontal) - curExtent;
 			if ( options.stickToCurrentElement && 
 				options.curElement.length > 0 && allElements.index($(this)) < curIndex )
 				refExtent += extent;
 		});
 
 		return refExtent;
+	}
+
+	function isVisible(){
+		return parseFloat($(this).css('opacity'),10) != 0.0;
+	}
+
+	function isHidden(){
+		return parseFloat($(this).css('opacity'),10) != 1.0;
 	}
 
 	// filter elements for according to given filter selector
@@ -105,60 +116,76 @@
 		hidden = allElements.not(options.filter);
 		shown = allElements.filter(options.filter);
 
-		var toShow = shown.not(':visible');
-		var toHide = hidden.filter(':visible');
+		allElements.stop(true,false);
+		if ( options.stickToCurrentElement )
+			options.container.stop(true,false);
+
+		var toShow = shown.filter(isHidden);
+		var toHide = hidden.filter(isVisible);
+
 		var elementsToShow = ( allElements.length != hidden.length );
+		var allHidden = ( allElements.filter(isVisible).length == 0 );
 
-		if ( elementsToShow ){
-			options.curElement = options.getNewCurElement(options.filter,options.curElement,shown);
-			refExtent = getRefExtent(allElements,toHide,toShow,options);
+		options.before(elementsToShow);
 
-			toHide.each(function(){
-				obj = {opacity: 0 };
-				extent = getElementExtent($(this),options.horizontal);
-				$(this).data('extent',extent);
+		options.curElement = options.getNewCurElement(options.filter,options.curElement,shown);
+		if ( options.stickToCurrentElement )
+			refExtent = getRefExtent(allElements,toHide,toShow,options) +
+				 getExtentCorrection(toShow,options);
+
+		toHide.each(function(){
+			obj = {opacity: 0 };
+			extent = getElementExtent($(this),options.horizontal);
+			if ( elementsToShow ){
 				if ( options.horizontal )
 					obj.width = 0;
 				else
 					obj.height = 0;
-				$(this).animate(obj,options.duration);
+			}
+
+			$(this).animate(obj,options.duration,function(){
+				setElementExtent($(this),0,options.horizontal);
 			});
-		}else
-			toHide.fadeOut(options.duration);
-
-		toHide.promise().done(function(){
-			toHide.hide();
 		});
-		
-		options.beforeShow(elementsToShow);
-		if ( !elementsToShow )
-			return;
-
-		toShow.show();
 
 		toShow.each(function(){
 			obj = {opacity: 1 };
 			extent = getElementExtent($(this),options.horizontal);
-			if ( options.horizontal )
-				obj.width = extent;
-			else
-				obj.height = extent;
+			if ( allHidden ){
+				setElementExtent($(this),extent,options.horizontal);
+			}else{
+				if ( options.horizontal )
+					obj.width = extent;
+				else
+					obj.height = extent;
+			}
+
 			$(this).animate(obj,options.duration,function(){
 				if ( this.style && this.style.removeAttribute)
 					this.style.removeAttribute("filter");
 			});
 		});
 
-		if ( options.stickToCurrentElement ){
-			refExtent += getExtentCorrection(toShow,options);
-			console.log('refext:'+refExtent);
-			if ( options.horizontal )
-				options.container.animate({left: "-="+ refExtent},options.duration);
-			else
-				options.container.animate({top: "-="+ refExtent},options.duration);
+		if ( options.stickToCurrentElement && elementsToShow ){
+			if ( allHidden ){
+				off = options.container.offset();
+				if ( options.horizontal )
+					off.left -= refExtent;
+				else
+					off.top -= refExtent;
+
+				options.container.offset(off);
+			}else{
+				if ( options.horizontal )
+					options.container.animate({left: "-="+ refExtent},options.duration);
+				else
+					options.container.animate({top: "-="+ refExtent},options.duration);
+			}
 		}
 
-		toShow.promise().done(options.afterShow);
+		allElements.promise().done(function(){
+			options.after(allElements.filter(isVisible).length > 0);
+		});
 
 		return options.curElement;
 	};
