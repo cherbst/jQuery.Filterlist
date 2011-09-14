a20739e31e65254e85bf05a2a6003aa005877fca
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-09-14 16:47:19 +0200

    new animation v1
    simpler and faster

--- jquery.filterlist.js
+++ jquery.filterlist.js
@@ -64,59 +64,6 @@
 		return (options.horizontal?closest.offset().left:closest.offset().top) - options.margin;
 	}
 
-	function findElement(element,allElements,toShow,hidden,prev){
-		var start = allElements.index(element) + (prev?0:1);
-		if ( start < 0 || start >= allElements.length ) return $();
-
-		var elements = allElements.slice((prev?0:start),(prev?start:allElements.length));
-		var newElement = $();
-		(prev?$(elements.get().reverse()):elements).each(function(){
-			if ( toShow.index($(this)) == -1 &&
-			     hidden.index($(this)) == -1 ){
-				newElement = $(this);
-				return false;
-			}
-		});
-		return newElement;
-	}
-
-	function findPrevElement(element,allElements,toShow,hidden){
-		return findElement(element,allElements,toShow,hidden,true);
-	}
-
-	function findNextElement(element,allElements,toShow,hidden){
-		return findElement(element,allElements,toShow,hidden,false);
-	}
-
-	function getElementId(element,allElements){
-		var element_id = element.attr('id'); 
-		if ( !element_id ){
-			element_id = 'element_'+allElements.index(element);
-			element.attr('id',element_id);
-		}
-		return element_id;
-	}
-
-	// returns the group id of the given element
-	function getGroupId(element,allElements,toShow,hidden){
-		var upper_id = "begin"; 
-		var lower_id = "end";
-
-		// find prev element from this not in toShow/toHide
-		var upper = findPrevElement(element,allElements,toShow,hidden);
-
-		if ( upper.length > 0 )
-			upper_id = getElementId(upper,allElements);
-
-		// find next element from this not in toShow/toHide
-		var lower = findNextElement(element,allElements,toShow,hidden);
-
-		if ( lower.length > 0 )
-			lower_id = getElementId(lower,allElements); 
-
-		return upper_id+'_'+lower_id;
-	}
-
 	// gets the extent of the element from its data field if possible
 	// this is done because getting the extent of a hidden element
 	// takes a long time
@@ -127,89 +74,32 @@
 		return extent;
 	}
 
-	function computeNewExtents(allElements,toHide,toShow,hidden,shown,options){
-		var groups = {};
-		// compute show/hide groups
-
-		var refExtent = 0;
+	function getRefExtent(allElements,toHide,toShow,options){
 		var curIndex = allElements.index(options.curElement);
-		var group_id;
-		var oldElement;
+		var refExtent = 0;
 
-		toHide.add(toShow).each(function(){
-			// get new group id if this is not the next of the old element	
-			if ( !oldElement || $(this).prev().length == 0 ||
-				getElementId(oldElement,allElements) != getElementId($(this).prev(),allElements) )
-				group_id = getGroupId($(this),allElements,toShow,hidden);
-
-			var group;
-			if ( group_id in groups )
-				group = groups[group_id];
-			else{
-				group = {
-					toHide : [],
-					toShow : [],
-					hideExtent: 0,
-					showExtent : 0
-				};
-				groups[group_id] = group;
-			}
-
-			var extent = getElementExtent($(this),options.horizontal);
-			if ( toShow.index($(this)) != -1 ){
-				if ( options.stickToCurrentElement && 
-					options.curElement.length > 0 && allElements.index($(this)) < curIndex )
-					refExtent += extent;
-				group.toShow.push($(this));
-				group.showExtent += extent;
-			}else{
-				if ( options.stickToCurrentElement && 
-					options.curElement.length > 0 && allElements.index($(this)) < curIndex )
-					refExtent -= extent;
-				$(this).data('newextent',extent);
-				group.toHide.push($(this));
-				group.hideExtent += extent;
-			}
-			oldElement = $(this);
+		toHide.each(function(){
+			extent = getElementExtent($(this),options.horizontal);
+			if ( options.stickToCurrentElement && 
+				options.curElement.length > 0 && allElements.index($(this)) < curIndex )
+				refExtent -= extent;
 		});
 
-		if ( options.stickToCurrentElement ){
-			refExtent += getExtentCorrection(toShow,options);
-			options.container.data('refExtent',refExtent);
-		}
+		toShow.each(function(){
+			extent = getElementExtent($(this),options.horizontal);
+			if ( options.stickToCurrentElement && 
+				options.curElement.length > 0 && allElements.index($(this)) < curIndex )
+				refExtent += extent;
+		});
 
-		for(key in groups) {
-			console.log('Group:'+key);
-			var group = groups[key];
-			var diff = group.hideExtent - group.showExtent;
-			if ( group.showExtent > group.hideExtent ){
-				for (var i = 0,j = group.toShow.length; i < j; i++) {
-					var element = group.toShow[i];
-					var extent = getElementExtent(element,options.horizontal);
-					element.data('extent',extent);
-					var newExtent = Math.round(Math.abs(
-						extent + (extent / group.showExtent)*diff));
-					if ( options.horizontal )
-						element.width(newExtent);
-					else
-						element.height(newExtent);
-				}
-			}else{
-				for (var i = 0,j = group.toHide.length; i < j; i++) {
-					var element = group.toHide[i];
-					var extent = getElementExtent(element,options.horizontal);
-					var newExtent = Math.round(Math.abs(
-						extent - (extent / group.hideExtent)*diff));
-					element.data('newextent',newExtent);
-				}
-			}
-		}
+		return refExtent;
 	}
 
 	// filter elements for according to given filter selector
 	function filterElements(options){
 		var allElements = this;
 		var hidden,shown, toShow;
+		var refExtent = 0;
 
 		// keep all elements matching filter
 		hidden = allElements.not(options.filter);
@@ -221,66 +111,54 @@
 
 		if ( elementsToShow ){
 			options.curElement = options.getNewCurElement(options.filter,options.curElement,shown);
-			computeNewExtents(allElements,toHide,toShow,hidden,shown,options);
+			refExtent = getRefExtent(allElements,toHide,toShow,options);
 
 			toHide.each(function(){
 				obj = {opacity: 0 };
-				ext = parseInt($(this).data('newextent'),10);
+				extent = getElementExtent($(this),options.horizontal);
+				$(this).data('extent',extent);
 				if ( options.horizontal )
-					obj.width = ext;
+					obj.width = 0;
 				else
-					obj.height = ext;
+					obj.height = 0;
 				$(this).animate(obj,options.duration);
 			});
-			if ( options.stickToCurrentElement ){
-				var refExtent = parseInt(options.container.data('refExtent'),10);
-				if ( refExtent < 0 ){
-					if ( options.horizontal )
-						options.container.animate({left: "-="+ refExtent},options.duration);
-					else
-						options.container.animate({top: "-="+ refExtent},options.duration);
-				}
-			}
 		}else
 			toHide.fadeOut(options.duration);
 
 		toHide.promise().done(function(){
-
 			toHide.hide();
-			toHide.each(function(){
-				$(this).css((options.horizontal?'width':'height'),getElementExtent($(this),options.horizontal));
-			});
-
-			options.beforeShow(elementsToShow);
-			if ( !elementsToShow )
-				return;
-
-			toShow.show();
-
-			toShow.each(function(){
-				obj = {opacity: 1 };
-				if ( options.horizontal )
-					obj.width = getElementExtent($(this),options.horizontal);
-				else
-					obj.height = getElementExtent($(this),options.horizontal);
-				$(this).animate(obj,options.duration,function(){
-					if ( this.style && this.style.removeAttribute)
-						this.style.removeAttribute("filter");
-				});
-			});
-
-			if ( options.stickToCurrentElement ){
-				var refExtent = parseInt(options.container.data('refExtent'),10);
-				if ( refExtent > 0 ){
-					if ( options.horizontal )
-						options.container.animate({left: "-="+ refExtent},options.duration);
-					else
-						options.container.animate({top: "-="+ refExtent},options.duration);
-				}
-			}
-
-			toShow.promise().done(options.afterShow);
 		});
+		
+		options.beforeShow(elementsToShow);
+		if ( !elementsToShow )
+			return;
+
+		toShow.show();
+
+		toShow.each(function(){
+			obj = {opacity: 1 };
+			extent = getElementExtent($(this),options.horizontal);
+			if ( options.horizontal )
+				obj.width = extent;
+			else
+				obj.height = extent;
+			$(this).animate(obj,options.duration,function(){
+				if ( this.style && this.style.removeAttribute)
+					this.style.removeAttribute("filter");
+			});
+		});
+
+		if ( options.stickToCurrentElement ){
+			refExtent += getExtentCorrection(toShow,options);
+			console.log('refext:'+refExtent);
+			if ( options.horizontal )
+				options.container.animate({left: "-="+ refExtent},options.duration);
+			else
+				options.container.animate({top: "-="+ refExtent},options.duration);
+		}
+
+		toShow.promise().done(options.afterShow);
 
 		return options.curElement;
 	};
